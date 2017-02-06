c85a00f2975465d3af91c4222b7361684867dad2
yabursev <yakovburtsev@gmail.com>
2017-02-06 15:59:34 +0300

    Add zuul

--- gateway/src/main/java/gateway/GatewayApplication.java
+++ gateway/src/main/java/gateway/GatewayApplication.java
@@ -1,10 +1,17 @@
 package gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by SBT-Burtsev-YA on 06.02.2017.
  */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
